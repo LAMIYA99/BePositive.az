e5d02a6fd37959d1fa05770f08682a674fb1aaf8
Marvan <marvansm@code.edu.az>
2025-11-23 17:40:58 +0400

    _

--- client/src/Featured/Sections/ReviewSection/index.tsx
+++ client/src/Featured/Sections/ReviewSection/index.tsx
@@ -1,9 +1,13 @@
+import ReviewCard from "@/Featured/Common/ReviewCards";
 import HeadingText from "../../Common/HeadingText";
 
 const ReviewSection = () => {
   return (
-    <div className="mt-[72px]">
+    <div className="mt-[72px] container mx-auto">
       <HeadingText title="Customer reviews" />
+      <div className="grid grid-cols-3 ">
+        <ReviewCard />
+      </div>
     </div>
   );
 };
