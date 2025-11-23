9629bbd19f944b35994afa17b66bfe481ac1965d
LAMIYA ALIZADA <lamiyaea@code.edu.az>
2025-11-23 18:35:51 +0400

    _

--- client/src/Featured/Sections/ServiceSection/index.tsx
+++ client/src/Featured/Sections/ServiceSection/index.tsx
@@ -5,7 +5,7 @@ const ServiceSection = () => {
   const cards = [1, 2, 3, 4, 5, 6];
 
   return (
-    <section className="container mx-auto py-10">
+    <section  className="container mx-auto py-10">
       <HeadingText title="Services" />
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-auto mt-10 px-4">
---
+++ client/src/Featured/Sections/TrainingSection/index.tsx
@@ -0,0 +1,8 @@
+
+const TrainingSection = () => {
+  return (
+    <div className="">TrainingSection</div>
+  )
+}
+
+export default TrainingSection
