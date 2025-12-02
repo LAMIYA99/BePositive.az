b1db4c0b043b03ef850ac74f609a943d443168ff
LAMIYA ALIZADA <lamiyaea@code.edu.az>
2025-12-02 19:09:32 +0400

    _

--- client/src/Featured/Sections/Home/BrandsSection/index.tsx
+++ client/src/Featured/Sections/Home/BrandsSection/index.tsx
@@ -1,3 +1,5 @@
+import Marquee from "react-fast-marquee";
+
 const BrandSection = () => {
   const images = [
     "/Ellipse 51.png",
@@ -6,17 +8,20 @@ const BrandSection = () => {
     "/Ellipse 62.png",
     "/Ellipse 64.png",
     "/Ellipse 65.png",
+    "/Ellipse 61.png",
     "/Ellipse 66.png",
+    "/Ellipse 62.png",
+    "/Ellipse 64.png",
     "/Ellipse 67.png",
   ];
 
   return (
-    <section data-aos="zoom-in-down" className="my-[72px] px-4 sm:px-6 md:px-10">
-      <div className="grid grid-cols-2 sm:grid-cols-4 gap-4 justify-items-center">
+    <section data-aos="zoom-in-down" className="my-[72px]">
+      <Marquee speed={50} gradient={false} pauseOnHover={false}>
         {images.map((src, index) => (
           <div
             key={index}
-            className="bg-[#F5F7FA] rounded-[99px] flex justify-center items-center p-4"
+            className="bg-[#F5F7FA] rounded-[99px] inline-block mx-4"
           >
             <img
               src={src}
@@ -25,7 +30,7 @@ const BrandSection = () => {
             />
           </div>
         ))}
-      </div>
+      </Marquee>
     </section>
   );
 };
