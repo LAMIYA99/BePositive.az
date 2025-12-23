7a7286d6d3445b871f05c635641e3347e42db3b4
LAMIYA ALIZADA <lamiyaea@code.edu.az>
2025-12-24 02:49:27 +0400

    Local changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ const authRoutes = require("./routes/authRoutes");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
-
 const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
@@ -58,7 +57,7 @@ const upload = multer({ storage });
 
 app.post("/api/upload", upload.single("image"), (req, res) => {
   if (!req.file) return res.status(400).json({ message: "No file uploaded" });
-  res.json({ url: `http://localhost:5000/uploads/${req.file.filename}` });
+  res.json({ url: `http://localhost:5001/uploads/${req.file.filename}` });
 });
 
 
