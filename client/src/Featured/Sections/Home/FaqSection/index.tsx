a5646beb3d49a5b9ca8b0a0548cdaa76b454d05e
Alamdar Manafov <alamdarmanafov@gmail.com>
2025-12-27 20:55:24 +0400

    Update index.tsx

--- client/src/Featured/Sections/Home/FaqSection/index.tsx
+++ client/src/Featured/Sections/Home/FaqSection/index.tsx
@@ -12,7 +12,7 @@ const FaqSection = () => {
 
   return (
     <div className="mt-[32px] lg:mt-[52px] container mx-auto px-6">
-      <HeadingText title={t({ en: "FAQ", az: "Tez-tez verilən suallar" })} />
+      <HeadingText title={t({ en: "FAG", az: "Tez-tez verilən suallar" })} />
       <div className="accordions mt-[13px] lg:mt-[53px]  flex flex-col gap-4 w-full">
         {accordionsData.map((acc: any) => (
           <Accordions key={acc.id} title={t(acc.title)} desc={t(acc.desc)} />
