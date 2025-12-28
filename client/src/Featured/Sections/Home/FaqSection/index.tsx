c3daff77eff02a93df58c831852056b65e464c74
Marvan S. Mammadli <marvansm@code.local>
2025-12-28 16:29:30 +0400

    _

--- client/src/Featured/Sections/Home/FaqSection/index.tsx
+++ client/src/Featured/Sections/Home/FaqSection/index.tsx
@@ -11,8 +11,8 @@ const FaqSection = () => {
     getTranslation(content, locale);
 
   return (
-    <div className="mt-[32px] lg:mt-[52px] container mx-auto px-6">
-      <HeadingText title={t({ en: "FAG", az: "Tez-tez verilən suallar" })} />
+    <div className="mt-8 lg:mt-[52px] container mx-auto px-6">
+      <HeadingText title={t({ en: "FAQ", az: "Tez-tez verilən suallar" })} />
       <div className="accordions mt-[13px] lg:mt-[53px]  flex flex-col gap-4 w-full">
         {accordionsData.map((acc: any) => (
           <Accordions key={acc.id} title={t(acc.title)} desc={t(acc.desc)} />
