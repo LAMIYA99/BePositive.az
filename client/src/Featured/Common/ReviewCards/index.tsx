3ea572b5e4312fff38c4177e993273f7537e34a6
Marvan <marvansm@code.edu.az>
2025-12-29 18:16:55 +0400

    Add push notification system with admin panel

--- client/src/Featured/Common/ReviewCards/index.tsx
+++ client/src/Featured/Common/ReviewCards/index.tsx
@@ -10,7 +10,7 @@ const ReviewCard = ({ review }: { review: any }) => {
     getTranslation(content, locale);
 
   return (
-    <div className="flex flex-col items-center justify-center gap-6 rounded-3xl bg-white lg:p-6 p-2 min-h-[352px]">
+    <div className="flex flex-col items-center justify-between rounded-3xl bg-white p-4 lg:p-6 min-h-[352px]">
       <div className="w-[76px] h-[76px] border-2 rounded-full overflow-hidden border-[#0808C1]">
         <Image
           src={review.avatar}
@@ -21,11 +21,13 @@ const ReviewCard = ({ review }: { review: any }) => {
         />
       </div>
 
-      <p className="text-[16px] text-[#364153] font-normal leading-[26px] tracking-[-0.312px] text-center w-[321px]">
-        "{t(review.text)}"
-      </p>
+      <div className="flex-1 flex items-center">
+        <p className="text-[16px] text-[#364153] font-normal leading-[26px] tracking-[-0.312px] text-center px-4 whitespace-normal break-words">
+          "{t(review.text)}"
+        </p>
+      </div>
 
-      <ul className="flex items-center flex-col">
+      <ul className="flex items-center flex-col mt-4">
         <li className="text-[#101828] text-[16px] font-normal leading-6 tracking-[-0.312px]">
           {t(review.name)}
         </li>
